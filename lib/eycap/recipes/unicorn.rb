64cba281d07961fbf3285811b88aabe9a1c2a5da
grant <grant@reki.local>
2009-12-16 16:59:30 -0600

    Changes to Unicorn tasks for new setup

--- lib/eycap/recipes/unicorn.rb
+++ lib/eycap/recipes/unicorn.rb
@@ -4,14 +4,14 @@ Capistrano::Configuration.instance(:must_exist).load do
     Start the Unicorn Master.  This uses the :use_sudo variable to determine whether to use sudo or not. By default, :use_sudo is set to true.
     DESC
     task :start, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/etc/init.d/unicorn_#{application} start"
+      sudo "/usr/bin/monit start all -g #{monit_group}"
     end
 
     desc <<-DESC
     Restart the Unicorn processes on the app server by starting and stopping the master. This uses the :use_sudo variable to determine whether to use sudo or not. By default, :use_sudo is set to true.
     DESC
     task :restart, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/etc/init.d/unicorn_#{application} restart"
+      sudo "/usr/bin/monit restart all -g #{monit_group}"
     end
 
     desc <<-DESC
@@ -20,40 +20,44 @@ Capistrano::Configuration.instance(:must_exist).load do
     set to true.
     DESC
     task :stop, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/etc/init.d/unicorn_#{application} stop"
+      sudo "/usr/bin/monit stop all -g #{monit_group}"
     end
 
     desc <<-DESC
-    Reloads the unicorn works gracefully - Should be used on deploy 
+    Reloads the unicorn works gracefully - Use deploy task for deploys
     DESC
     task :reload, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/etc/init.d/unicorn_#{application} reload"      
+      sudo "/engineyard/bin/unicorn #{application} reload"
     end
     
     desc <<-DESC
     Adds a Unicorn worker - Beware of causing your host to swap, this setting isn't permanent
     DESC
-    task :reload, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/etc/init.d/unicorn_#{application} aworker"      
+    task :aworker, :roles => [:app], :except => {:unicorn => false} do
+      sudo "/engineyard/bin/unicorn #{application} aworker"
     end
     
     desc <<-DESC
     Removes a unicorn worker (gracefully) 
     DESC
-    task :reload, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/etc/init.d/unicorn_#{application} rworker"      
+    task :rworker, :roles => [:app], :except => {:unicorn => false} do
+      sudo "/engineyard/bin/unicorn #{application} rworker"
     end
+    
+    desc <<-DESC
+    Deploys app gracefully with USR2 and unicorn.rb combo
+    DESC
+    task :deploy, :roles => [:app], :except => {:unicorn => false} do
+      sudo "/engineyard/bin/unicorn #{application} deploy"
+    end
+    
+    
   end #namespace
   
   namespace :deploy do
     desc "Restart the Unicorn processes on the app slices."
     task :restart, :roles => :app do
-      unicorn.reload
-    end
-
-    desc "Start the Unicorn processes on the app slices."
-    task :spinner, :roles => :app do
-      unicorn.start
+      unicorn.deploy
     end
 
     desc "Start the Unicorn processes on the app slices."
